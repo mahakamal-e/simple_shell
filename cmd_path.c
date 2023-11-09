e8b3959484bfb1478195ed3c210e02c5fd88dc3d
mahakamal-e <mahakamal09@gmail.com>
2023-11-08 16:53:39 -0800

    upload file

---
+++ cmd_path.c
@@ -0,0 +1,26 @@
+#include "shell.h"
+/**
+ * path_command - function that handle path for excutable command
+ *
+ * @cmd_: parsed input(command)
+ * @envp: array of envp
+ * Return: a pointer to a null-terminated string representing the full path
+ * of the executable file that corresponds to the given command name.
+ */
+char *path_command(char *cmd_, char __attribute__((unused)) **envp)
+{
+	char *path_cmd;
+	int iterat = 0;
+
+	while (cmd_[iterat])
+	{
+		if (cmd_[iterat] == '/')
+			return (searchInDirectory(cmd_));
+		iterat++;
+	}
+	path_cmd = _getenv("PATH", envp);
+	if (path_cmd == NULL)
+		return (NULL);
+	
+	return (searchInPath(cmd_, path_cmd));
+}
