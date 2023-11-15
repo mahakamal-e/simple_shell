52db076b220f964f4bd24c4476535efdf5c9e1fc
deenakamal <deenah.kamal@gmail.com>
2023-11-15 04:51:53 -0800

    f

--- handle_command.c
+++ handle_command.c
@@ -4,7 +4,6 @@
  *
  * @_words: spliting input line to words
  * @argv: array of args
- * @ex_st: status to exit with
  * @cmd_c: cmd count
  * @env: array of enviroments
  *
@@ -12,13 +11,13 @@
  */
 void handle_comm(char **_words, char **argv, int cmd_c, char **env)
 {
-	if (strcmp(_words[0], "exit") == 0) {
-        printf("Exiting the shell...\n");
-        exit(EXIT_SUCCESS);
-    }
+	if (_my_strcmp(_words[0], "exit") == 0)
+	{
+		exit(EXIT_SUCCESS);
+	}
 	else
 	{
-	excute_command(_words, argv, env, cmd_c);
+		excute_command(_words, argv, env, cmd_c);
 	}
 
 }
